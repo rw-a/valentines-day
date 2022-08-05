50529cfd85fae38e66c1fe56d2bcfa0d592f8d8b
rw-a <bshsphotodump@gmail.com>
2022-08-05 17:23:38 +1000

    renamed purchase to redeem

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -6,12 +6,12 @@ from . import views
 app_name = 'ticketing'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('redeem/', views.purchase, name='purchase'),
-    path('redeemed/', views.purchased, name='purchased'),
+    path('redeem/', views.redeem, name='redeem'),
+    path('redeemed/', views.redeemed, name='redeemed'),
     path('validate_code/', views.validate_code, name='validate_code'),
     path('validate_recipient/', views.validate_recipient, name='validate_recipient'),
     path('codes/<int:pk>', views.codepdf, name='codepdf'),
     path('generator/', views.generator, name='generator'),
     path('generator/<str:code>/', views.generator),
-    path('sort/', views.stats, name='stats')
+    path('sort/', views.sort, name='sort')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
