c06d69e7e9c8af267d14857bcee119f58de91e11
rw-a <108516417+rw-a@users.noreply.github.com>
2023-01-07 21:46:34 +1000

    Update settings.py

--- vdaywebsite/settings.py
+++ vdaywebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os.path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v8-xi^&0-5t^5vuzpwv_tw!$)7r77(hy8lt84(4s*$as3h=p+1'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
