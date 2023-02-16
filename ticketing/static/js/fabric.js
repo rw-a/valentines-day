28120e4db9213a47168484af4ffd816aae238cbe
rw-a <bshsphotodump@gmail.com>
2023-02-16 15:05:54 +1000

    added reset button and fixed undo

--- ticketing/static/js/fabric.js
+++ ticketing/static/js/fabric.js
@@ -1,30 +1,33 @@
 /* Loading Template */
 function load_background_image(template_src) {
-    fabric.Image.fromURL(template_src, function(img) {
-        fabric_canvas.setBackgroundImage(img, fabric_canvas.renderAll.bind(fabric_canvas), {
-            scaleX: fabric_canvas.width / img.width,
-            scaleY: fabric_canvas.height / img.height
+    return new Promise((resolve) => {
+        fabric.Image.fromURL(template_src, (img) => {
+            fabric_canvas.setBackgroundImage(img, () => {
+                fabric_canvas.renderAll.bind(fabric_canvas);
+                resolve();
+            }, {
+                scaleX: fabric_canvas.width / img.width,
+                scaleY: fabric_canvas.height / img.height
+            });
         });
-    });
+    })
 }
 
-function initialise_template() {
+async function initialise_template() {
     fabric_canvas.clear();
-    load_font();
+
     let template = document.getElementById('typed_template').value;
     if (template === "Blank") {
         const texts = [
             new fabric.IText('Placeholder', {"left": 18, "top": 20, "fontSize": 30}),
-            new fabric.IText('Placeholder', {"left": 18, "top": 80, "fontSize": 30}),
-            new fabric.IText('Placeholder', {"left": 18, "top": 140, "fontSize": 30}),
-            new fabric.IText('Placeholder', {"left": 18, "top": 200, "fontSize": 30}),
-            new fabric.IText('Placeholder', {"left": 18, "top": 260, "fontSize": 30})
+            new fabric.IText('Placeholder', {"left": 18, "top": 120, "fontSize": 30}),
+            new fabric.IText('Placeholder', {"left": 18, "top": 220, "fontSize": 30})
         ];
         fabric_canvas.add(...texts);
     } else if (Object.keys(templates).includes(template)) {
         const filename = (templates[template].filenameRastered) ? templates[template].filenameRastered : templates[template].filename;
         const filePath = `${static_path}templates/${filename}`;
-        load_background_image(filePath);
+        await load_background_image(filePath);
         let texts = [];
         for (let text_info of templates[template].textPosition) {
             texts.push(new fabric.IText('Placeholder', text_info));
@@ -33,30 +36,39 @@ function initialise_template() {
     } else {
         console.error(`Template ${template} not found.`);
     }
+    await load_font();
+    save_fabric();
 }
 
-document.getElementById('typed_template').addEventListener('change', (event) => {
-    initialise_template();
-})
+document.getElementById('typed_template').addEventListener('change', async (event) => {
+    await initialise_template();
+});
 
 
-/* Undo & Clear */
+/* Undo, Reset & Clear */
 let fabric_canvas_data;
 let undo_history = [];
 
-function save_fabric() {       // this should not be called by itself. only called by load font and event listeners
-                               // initial call won't have a state
+function save_fabric() {    // saves the canvas state into undo history
     if (fabric_canvas_data) {
         undo_history.push(fabric_canvas_data);
         document.getElementById('fabric_undo').disabled = false;
     }
     fabric_canvas_data = JSON.stringify(fabric_canvas);
+    console.log(fabric_canvas_data);
 }
+
+// reset button
+document.getElementById("fabric_reset").addEventListener("click", async () => {
+    document.getElementById('tooManyTextBoxesError').hidden = true;
+    await initialise_template();
+});
 
 // clear button
 document.getElementById("fabric_clear").addEventListener("click", () => {
     document.getElementById('tooManyTextBoxesError').hidden = true;
-    initialise_template();
+    fabric_canvas.remove(...fabric_canvas.getObjects());  // delete all objects but not background
+    save_fabric();
 });
 
 // undo button
@@ -64,6 +76,7 @@ document.getElementById("fabric_undo").addEventListener("click", () => {
     document.getElementById('tooManyTextBoxesError').hidden = true;
     if (undo_history.length > 0) {
         fabric_canvas_data = undo_history.pop();
+        console.log(fabric_canvas_data);
         this.disabled = true;
         fabric_canvas.clear();
         fabric_canvas.loadFromJSON(fabric_canvas_data, function() {
@@ -81,21 +94,12 @@ fabric_canvas.on('object:modified', function() {
 });
 
 /* Load Fonts */
-function load_font(font) {
-    if (font === undefined) {
-        font = document.getElementById("font_selector").value;
-    }
-
+async function load_font() {
+    const font = document.getElementById("font_selector").value;
     let myFont = new FontFaceObserver(font);
-    myFont.load()
-        .then(function() {
-            fabric_canvas.getObjects().forEach(function(object) {object.set("fontFamily", font);})
-            fabric_canvas.requestRenderAll();
-            save_fabric();
-        }).catch(function(e) {
-            console.log(e);
-            alert('Failed to load font: ' + font);
-    });
+    await myFont.load();
+    fabric_canvas.getObjects().forEach(function(object) {object.set("fontFamily", font);})
+    fabric_canvas.requestRenderAll();
 }
 
 const font_selector = document.getElementById("font_selector");
@@ -122,8 +126,8 @@ for (let font of Object.keys(fonts)) {
 }
 document.head.appendChild(fontStyles);
 
-font_selector.onchange = function() {
-    load_font();
+font_selector.onchange = async function() {
+    await load_font();
 }
 
 initialise_template();
@@ -148,6 +152,7 @@ function deleteObject(eventData, transform) {
     canvas.remove(target);
     canvas.requestRenderAll();
     document.getElementById('tooManyTextBoxesError').hidden = true;
+    save_fabric();
 }
 
 fabric.Object.prototype.controls.deleteControl = new fabric.Control({
