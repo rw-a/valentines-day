f9d5e6ac2ea1d31eacc3bab48906639aff3d78f4
rw-a <bshsphotodump@gmail.com>
2024-01-27 12:00:21 +1000

    format: shorten lines to fit within line length

--- ticketing/models.py
+++ ticketing/models.py
@@ -6,9 +6,10 @@ from .constants import MaxLengths, STUDENTS, TEMPLATES
 
 
 class TicketCodePDF(models.Model):
-    num_of_items = models.PositiveIntegerField(default=100, validators=[MinValueValidator(1), MaxValueValidator(3000)],
-                                               help_text="The number of codes you want to generate. "
-                                                         "Multiple of 100 recommended.")
+    num_of_items = models.PositiveIntegerField(
+        default=100, validators=[MinValueValidator(1), MaxValueValidator(3000)],
+        help_text="The number of codes you want to generate. Multiple of 100 recommended."
+    )
 
     item_type = models.CharField(
         max_length=20,
@@ -47,11 +48,11 @@ class TicketCode(models.Model):
     )
 
     # links ticket codes to the pdf which created them. can also be null if created individually
-    pdf = models.ForeignKey(TicketCodePDF, on_delete=models.SET_NULL, null=True, blank=True,
-                            verbose_name="Corresponding TicketCodePDF (optional)",
-                            help_text="Links the code to the PDF which generated it. "
-                                      "Leave it blank if you are manually creating the code "
-                                      "(which you probably shouldn't be doing anyways).")
+    pdf = models.ForeignKey(
+        TicketCodePDF, on_delete=models.SET_NULL, null=True, blank=True,
+        verbose_name="Corresponding TicketCodePDF (optional)",
+        help_text="Links the code to the PDF which generated it. Leave it blank if you are "
+                  "manually creating the code (which you probably shouldn't be doing anyway).")
 
     date = models.DateTimeField(default=timezone.now, help_text="Date created")
 
@@ -71,10 +72,11 @@ class TicketCode(models.Model):
 
 
 class Ticket(models.Model):
-    recipient_id = models.CharField(max_length=100,
-                                    verbose_name="Recipient ID",
-                                    help_text="A unique identifier for each student. "
-                                              "Is represented by their full name and ARC class.")
+    recipient_id = models.CharField(
+        max_length=100, verbose_name="Recipient ID",
+        help_text="A unique identifier for each student. "
+                  "Is represented by their full name and ARC class.")
+
     item_type = models.CharField(
         max_length=20,
         choices=[
@@ -87,33 +89,41 @@ class Ticket(models.Model):
     )
     template = models.CharField(max_length=100)
 
-    ss_period = models.PositiveIntegerField(null=True, blank=True, verbose_name="Special Serenade Period",
-                                            help_text="The period that the special serenade is requested to be in.")
+    ss_period = models.PositiveIntegerField(
+        null=True, blank=True, verbose_name="Special Serenade Period",
+        help_text="The period that the special serenade is requested to be in."
+    )
 
     is_handwritten = models.BooleanField(default=False)
 
     # links ticket to the code which made it. can also be null if it was manually created by prefect
-    code = models.OneToOneField(TicketCode, on_delete=models.SET_NULL, null=True, blank=True,
-                                verbose_name="Corresponding TicketCode (optional)",
-                                help_text="Links the ticket to the code which made it. "
-                                          "Leave it blank if you are manually creating the ticket.")
+    code = models.OneToOneField(
+        TicketCode, on_delete=models.SET_NULL, null=True, blank=True,
+        verbose_name="Corresponding TicketCode (optional)",
+        help_text="Links the ticket to the code which made it. "
+                  "Leave it blank if you are manually creating the ticket.")
 
-    period = models.PositiveIntegerField(null=True, blank=True, editable=False,
-                                         help_text="The period chosen by the ticket sorter. "
-                                                   "Will be automatically determined so do not touch.")
-    p1 = models.CharField(null=True, blank=True, max_length=4, editable=False, verbose_name="Period 1 Classroom",
-                          help_text="Will be automatically determined so do not touch.")
-    p2 = models.CharField(null=True, blank=True, max_length=4, editable=False, verbose_name="Period 2 Classroom",
-                          help_text="Will be automatically determined so do not touch.")
-    p3 = models.CharField(null=True, blank=True, max_length=4, editable=False, verbose_name="Period 3 Classroom",
-                          help_text="Will be automatically determined so do not touch.")
-    p4 = models.CharField(null=True, blank=True, max_length=4, editable=False, verbose_name="Period 4 Classroom",
-                          help_text="Will be automatically determined so do not touch.")
+    period = models.PositiveIntegerField(
+        null=True, blank=True, editable=False,
+        help_text="The period chosen by the ticket sorter. "
+                  "Will be automatically determined so do not touch.")
+    p1 = models.CharField(
+        null=True, blank=True, max_length=4, editable=False, verbose_name="Period 1 Classroom",
+        help_text="Will be automatically determined so do not touch.")
+    p2 = models.CharField(
+        null=True, blank=True, max_length=4, editable=False, verbose_name="Period 2 Classroom",
+        help_text="Will be automatically determined so do not touch.")
+    p3 = models.CharField(
+        null=True, blank=True, max_length=4, editable=False, verbose_name="Period 3 Classroom",
+        help_text="Will be automatically determined so do not touch.")
+    p4 = models.CharField(
+        null=True, blank=True, max_length=4, editable=False, verbose_name="Period 4 Classroom",
+        help_text="Will be automatically determined so do not touch.")
 
-    sort_order = models.PositiveIntegerField(null=True, blank=True, editable=False,
-                                             help_text="Used to determine what order the tickets "
-                                                       "should be when printing. "
-                                                       "Will be automatically determined so do not touch.")
+    sort_order = models.PositiveIntegerField(
+        null=True, blank=True, editable=False,
+        help_text="Used to determine what order the tickets should be when printing. "
+                  "Will be automatically determined so do not touch.")
 
     date = models.DateTimeField(default=timezone.now, help_text="Date created")
 
@@ -139,32 +149,13 @@ class Ticket(models.Model):
 
 
 class SortTicketsRequest(models.Model):
-    num_serenaders = models.IntegerField(default=10, validators=[MaxValueValidator(100), MinValueValidator(1)],
-                                         verbose_name="Number of serenading delivery groups")
-    num_non_serenaders = models.IntegerField(default=10, validators=[MaxValueValidator(100), MinValueValidator(1)],
-                                             verbose_name="Number of non-serenading delivery groups")
-    max_serenades_per_class = models.IntegerField(default=2, verbose_name="Max number of serenades per class visit",
-                                                  help_text="Lower values increase number of class visits required and "
-                                                            "shifts load towards serenading groups. "
-                                                            "Set to 0 to disable limit.")
-    max_non_serenades_per_serenading_class = models.IntegerField(default=3,
-                                                                 verbose_name="Max number of non-serenades per "
-                                                                              "class visit",
-                                                                 help_text="Only enforced for classes with at least "
-                                                                           "one serenade. Lower values increase number "
-                                                                           "of class visits required and shifts load "
-                                                                           "towards non-serenading groups. Set to 0 to "
-                                                                           "disable limit.")
-    extra_special_serenades = models.BooleanField(default=True,
-                                                  help_text="Special serenades will not be grouped with regular "
-                                                            "serenades. Can be guaranteed but "
-                                                            "some classes may have to be visited twice. "
-                                                            "Increases number of class visits required.")
-    enforce_distribution = models.BooleanField(default=True,
-                                               help_text="Splits up serenades between periods. "
-                                                         "Ensures that people do not receive multiple serenades at "
-                                                         "once (no promises). "
-                                                         "Increases number of class visits required.")
+    num_serenaders = models.IntegerField(
+        default=10, validators=[MaxValueValidator(100), MinValueValidator(1)],
+        verbose_name="Number of serenading delivery groups")
+    num_non_serenaders = models.IntegerField(
+        default=10, validators=[MaxValueValidator(100), MinValueValidator(1)],
+        verbose_name="Number of non-serenading delivery groups")
+
 
     date = models.DateTimeField(default=timezone.now, help_text="Date created")
 
@@ -174,7 +165,7 @@ class SortTicketsRequest(models.Model):
 
 
 class DeliveryGroup(models.Model):
-    code = models.CharField(max_length=3)
+    code = models.CharField(max_length=10)
     is_serenading_group = models.BooleanField()
     parts_printed = models.TextField()    # the PDF parts which have been printed as a comma delimited str (e.g. "1,2")
 
@@ -185,4 +176,3 @@ class DeliveryGroup(models.Model):
 
     class Meta:
         verbose_name = "Delivery Group"
-        verbose_name_plural = "Delivery Groups"
